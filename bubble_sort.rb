c1b940383656f56cd3c7d07b425ea576f8e24978
ybadmus <yusif.badmus@gmail.com>
2021-01-21 10:06:04 +0000

    add while loop, bubble sort complete

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,29 @@
+require 'pry'
+
 def bubble_sort (arr)
     count_while = 0
     count_for = arr.length - 1
 
+    while count_while < arr.length do
+        swapped = false
+        binding.pry
 
+        count_for.times do |i|
+            if arr[i] > arr[i + 1]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = true
+            end
+        end
+
+        binding.pry
+
+        count_for -= 1
+        count_while += 1
+
+        unless swapped
+            break
+        end
+    end
     
     p arr
 end
