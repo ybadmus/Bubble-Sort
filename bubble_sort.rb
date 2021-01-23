2a979c2c9bef6282b38beed9dab4a8cf9628d802
ybadmus <yusif.badmus@gmail.com>
2021-01-23 17:42:33 +0000

    resolve all rubocop issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,52 +1,46 @@
 require 'pry'
 
-def bubble_sort (arr)
-    count_while = 0
-    count_for = arr.length - 1
+def bubble_sort(arr)
+  count_while = 0
+  count_for = arr.length - 1
 
-    while count_while < arr.length do
-        swapped = false
+  while count_while < arr.length
+    swapped = false
 
-        count_for.times do |i|
-            if arr[i] > arr[i + 1]
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                swapped = true
-            end
-        end
-
-        count_for -= 1
-        count_while += 1
-
-        unless swapped
-            break
-        end
+    count_for.times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
     end
-    
-    arr
+
+    count_for -= 1
+    count_while += 1
+
+    break unless swapped
+  end
+  arr
 end
 
+def bubble_sort_by(arr)
+  count_while = 0
+  count_for = arr.length - 1
 
-def bubble_sort_by (arr)
-    count_while = 0
-    count_for = arr.length - 1
+  while count_while < arr.length
+    swapped = false
 
-    while count_while < arr.length do
-        swapped = false
-
-        count_for.times do |i|
-            if yield(arr[i], arr[i + 1]).positive?
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                swapped = true
-            end
-        end
-
-        count_for -= 1
-        count_while += 1
-
-        unless swapped
-            break
-        end
+    count_for.times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
     end
 
-    arr
+    count_for -= 1
+    count_while += 1
+
+    break unless swapped
+  end
+
+  arr
 end
